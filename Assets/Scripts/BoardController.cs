51a1550ac3e674425a90fd4f457d6f0438ca7465
Kotavy <tavius.koktavy@gmail.com>
2019-03-09 16:08:44 -0500

    Update scripts to pass tiered scores

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardController : MonoBehaviour
+{
+    private int score;
+    private int highScore;
+    public Text scoreText;
+    public Text highScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        highScore = 0;
+    }
+
+    public void UpdateScore(int newPoints)
+    {
+        Debug.Log(newPoints);
+        score += newPoints;
+
+        if (score > highScore)
+        {
+            highScore = score;
+        }
+        showText();
+    }
+
+    private void showText ()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        highScoreText.text = "High Score: " + highScore.ToString();
+    }
+}
+
---
+++ Assets/Scripts/PointsAssigner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsAssigner : MonoBehaviour
+{
+    public int pointsValue;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Dart"))
+        {
+            this.gameObject.GetComponentInParent<BoardController>().UpdateScore(pointsValue);
+        }
+    }
+}
