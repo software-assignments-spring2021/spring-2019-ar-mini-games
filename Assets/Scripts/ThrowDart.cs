53779d2be9e4b8007d07fc88ec30cffb7d7cf536
charleswdickstein <charleswdickstein@gmail.com>
2019-03-25 23:09:57 -0400

    removed xcode build

--- Assets/Scripts/ThrowDart.cs
+++ Assets/Scripts/ThrowDart.cs
@@ -21,7 +21,9 @@ public class ThrowDart : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("pressed");
+            rb.useGravity = true;
             rb.AddForce(transform.forward * thrust);
+            Invoke("DestroyDart", 3);
         }
 
 
@@ -35,24 +37,32 @@ public class ThrowDart : MonoBehaviour
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "DartBoard")
-        {
+        {   
+            rb.velocity = Vector3.zero;
+
             Debug.Log("Hit DartBoard!");
 
             // make dart "stick" to board by turning off gravity, movement, rotation
             rb.useGravity = false;
-            rb.velocity = Vector3.zero;
             rb.freezeRotation = true;
 
-            Invoke("DestroyDart", 2);
+            //Invoke("DestroyDart", 2);
 
         } // end of if dartboard
+
+
 
     }
 
 
     void DestroyDart()
     {
-        Instantiate(this.gameObject, new Vector3(0, 0.25f, -4f), Quaternion.identity);
+        Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f,0.8f)); 
+       
+        GameObject dartCpy = Instantiate(this.gameObject, p, Camera.main.transform.rotation);
+        dartCpy.transform.parent = GameObject.FindWithTag("MainCamera").transform;
+        dartCpy.GetComponent<Rigidbody>().useGravity = true;
+
         Destroy(this.gameObject);
 
 
