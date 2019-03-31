2af7dbf4796db487d4da81f4a23089c6021f13fe
charleswdickstein <charleswdickstein@gmail.com>
2019-03-31 17:14:24 -0400

    editorscript changes

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
 public class MyEditorScript : MonoBehaviour
 
     // Start is called before the first frame update
     {
         static string[] SCENES = FindEnabledEditorScenes();
 
-        static string APP_NAME = "ar-mini-games";
+        static string APP_NAME = "YourProject";
         static string TARGET_DIR = "target";
 
         [MenuItem("Custom/CI/Build Mac OS X")]
         static void PerformMacOSXBuild()
         {
             string target_dir = APP_NAME + ".app";
-            GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+            GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSX, BuildOptions.None);
         }
 
         private static string[] FindEnabledEditorScenes()
@@ -33,12 +32,11 @@ public class MyEditorScript : MonoBehaviour
 
         static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
         {
-            EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-            //string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
-            //if (res.Length > 0)
-            //{
-            //    throw new Exception("BuildPlayer failure: " + res);
-            //}
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, build_target);
+           
+
+        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options).ToString();
+
         }
 
 }
