ddd8d227400cbcacce69e4102bce81dbcc5e597c
charleswdickstein <charleswdickstein@gmail.com>
2019-04-03 17:18:49 -0400

    added real dart

--- Assets/Scripts/ThrowDart.cs
+++ Assets/Scripts/ThrowDart.cs
@@ -6,7 +6,6 @@ public class ThrowDart : MonoBehaviour
 {
     private Rigidbody rb;
     public int thrust;
-    private int dartCounter = 0;
 
     // Start is called before the first frame update
     void Start()
