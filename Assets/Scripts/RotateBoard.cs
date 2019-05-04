022ec018576c788572ec46e637da25e8fc2a16b4
asunwoo98 <antony342007@gmail.com>
2019-05-04 17:34:01 -0400

    adding rotating board, and background to pick up from camera

---
+++ Assets/Scripts/RotateBoard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBoard : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(30, 30, 30) * Time.deltaTime);
+    }
+}
