0b44a31f1fdb46f9bbecd1da19497b2c7cc908dc
charleswdickstein <charleswdickstein@gmail.com>
2019-05-05 17:39:15 -0400

    fix score, classic mode fixes

--- Assets/Scripts/PointsAssigner.cs
+++ Assets/Scripts/PointsAssigner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PointsAssigner : MonoBehaviour
 {
     public int pointsValue;
-    public GameObject DartBoard; 
+
 
     void OnCollisionEnter(Collision collision)
     {
