7842e9a58faceb3315c74d2151607fe1409eae86
asunwoo98 <antony342007@gmail.com>
2019-05-06 19:01:58 -0400

    adding timer functionality and related script, 'infinite' darts

---
+++ Assets/TimeTrialManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTrialManager : DartCounter
+{
+    public Text timer;
+    public float timeLimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeLeft = timeLimit - Time.fixedTime;
+        timer.text = (timeLeft).ToString("F2");
+        if(timeLeft< 0.0)
+        {
+            timer.text = "0.0";
+            gameOverPanel.SetActive(true);
+        }
+    }
+}
