82fe15f7b36923786cbb701a8da1fc502aef3889
charleswdickstein <charleswdickstein@gmail.com>
2019-05-08 19:55:52 -0400

    time restarts along with scene

--- Assets/TimeTrialManager.cs
+++ Assets/TimeTrialManager.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class TimeTrialManager : DartCounter
 {
     public Text timer;
-    public float timeLimit;
-    // Start is called before the first frame update
-    void Start()
+    float timeLimit = 30.0f;
+    float timeLeft = 30.0f;
+    private void Start()
     {
-        
+        timeLimit = 30.0f;
+        timeLeft = 30.0f;
     }
+
 
 
     // Update is called once per frame
     void Update()
     {
-        float timeLeft = timeLimit - Time.fixedTime;
+        float timeLeft = timeLimit - Time.timeSinceLevelLoad;
         timer.text = (timeLeft).ToString("F2");
         if(timeLeft< 0.0)
         {
